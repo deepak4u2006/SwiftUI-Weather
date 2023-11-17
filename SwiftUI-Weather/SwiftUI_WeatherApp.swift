c40f8f68f7dd5997a4ec0aabf8c2e3190d71bbca
Vishwa Deepak <deepak4u2006@gmail.com>
2023-11-17 20:18:20 +0530

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Vishwa Deepak Choudhary on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
